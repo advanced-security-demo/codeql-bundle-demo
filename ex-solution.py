13cf8b03fe210a4c8c8625a6c801601dc82a68cc
Charles Neill <charles.neill@rackspace.com>
2015-07-06 15:21:08 -0500

    removing selenium stuff

--- ex-solution.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'adri7917'
-from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser.get('http://localhost:8888/')
-
-search_element = browser.find_element_by_css_selector('input[type="submit"]')
-search_element.click()
--- example1.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'adri7917'
-
-from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser.get('http://google.com')
