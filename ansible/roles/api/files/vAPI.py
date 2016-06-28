8dcedfa8464555a1896373a1715570be9f09189c
Rahul Nair <rahulunair@gmail.com>
2016-06-28 10:46:20 -0500

    Added CORS support and fixed a few pep8 issues
    Added CORS support with enable_cors() method, all responses
    will have CORS enabled by default. Also a few pep8 issues was
    fixed.

--- ansible/roles/api/files/vAPI.py
+++ ansible/roles/api/files/vAPI.py
@@ -20,6 +20,7 @@ import re
 import xml.etree.ElementTree as ET
 from lxml import etree
 from bottle import route, run, request, debug
+from bottle import hook
 
 
 @route('/', method='GET')
@@ -28,12 +29,13 @@ def get_root():
     Give default message for a GET on root directory.
     '''
     response = {'response':
-        {
-            'Application': 'vulnerable-api',
-            'Status': 'running'
-        }
-    }
+                {
+                    'Application': 'vulnerable-api',
+                    'Status': 'running'
+                }
+                }
     return json.dumps(response, sort_keys=True, indent=2)
+
 
 @route('/tokens', method='POST')
 def get_token():
@@ -62,51 +64,70 @@ def get_token():
     c = conn.cursor()
     # no data validation
     # no sql parameterization
-    user_query = "SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (username, password)
+    user_query = "SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (
+        username, password)
     c.execute(user_query)
     user = c.fetchone()
     response = {}
     if user:
         response['access'] = {}
         response['access']['user'] = {'id': user[0], 'name': user[1]}
-        # make sure to get most recent token in database, because we arent removing them...
-        token_query = "SELECT * FROM tokens WHERE userid = '%s' ORDER BY expires DESC" % (user[0])
+        # make sure to get most recent token in database, because we arent
+        # removing them...
+        token_query = "SELECT * FROM tokens WHERE userid = '%s' ORDER BY expires DESC" % (user[
+                                                                                          0])
         c.execute(token_query)
         token_record = c.fetchone()
-        if type(token_record) == tuple:
+        if isinstance(token_record, tuple):
             if token_record[3] < int(time.time()):
-                # token has expired. create new one that expires 5 minutes after creation
-                expire_stamp = int(time.time()+300)
+                # token has expired. create new one that expires 5 minutes
+                # after creation
+                expire_stamp = int(time.time() + 300)
                 expire_date = time.ctime(int(expire_stamp))
                 token = hashlib.md5(expire_date).hexdigest()
-                # we'll parameterize this one because we need this serious functionality
-                c.execute('INSERT INTO tokens (token, userid, expires) VALUES (?, ?, ?)', (token, user[0], expire_stamp))
+                # we'll parameterize this one because we need this serious
+                # functionality
+                c.execute(
+                    'INSERT INTO tokens (token, userid, expires) VALUES (?, ?, ?)',
+                    (token,
+                     user[0],
+                        expire_stamp))
                 conn.commit()
-                response['access']['token'] = {'id': token, 'expires': expire_date}
+                response['access']['token'] = {
+                    'id': token, 'expires': expire_date}
             else:
                 # recent token hasn't expired. use same one.
                 expire_date = time.ctime(int(token_record[3]))
-                response['access']['token'] = {'id': token_record[1], 'expires': expire_date}
+                response['access']['token'] = {
+                    'id': token_record[1], 'expires': expire_date}
         else:
             # no token exists. create one that expires in 5 minutes
-            expire_stamp = int(time.time()+300)
+            expire_stamp = int(time.time() + 300)
             expire_date = time.ctime(int(expire_stamp))
             token = hashlib.md5(expire_date).hexdigest()
-            # we'll parameterize this one because we need this serious functionality
-            c.execute('INSERT INTO tokens (token, userid, expires) VALUES (?, ?, ?)', (token, user[0], expire_stamp))
+            # we'll parameterize this one because we need this serious
+            # functionality
+            c.execute(
+                'INSERT INTO tokens (token, userid, expires) VALUES (?, ?, ?)',
+                (token,
+                 user[0],
+                    expire_stamp))
             conn.commit()
             response['access']['token'] = {'id': token, 'expires': expire_date}
     else:
-        # let's do another look up so we can return helpful info for failure cases
+        # let's do another look up so we can return helpful info for failure
+        # cases
         c.execute("SELECT * FROM users WHERE username = '%s'" % username)
         user = c.fetchone()
         if user:
             response['error'] = {'message': 'password does not match'}
         else:
-            response['error'] = {'message': 'username ' + username + ' not found'}
+            response['error'] = {
+                'message': 'username ' + username + ' not found'}
     conn.close()
 
     return {json.dumps(response)}
+
 
 @route('/tokens', method='GET')
 def get_get_token():
@@ -120,6 +141,7 @@ def get_get_token():
     c.execute(query)
     users = c.fetchall()
     return {'response': users}
+
 
 @route('/user/<user:re:.*>', method='GET')
 def get_user(user):
@@ -139,33 +161,37 @@ def get_user(user):
     c.close()
     response = {}
     # you'll notice we don't actually check the token expiration date
-    if type(token_record) == tuple:
-        if type(user_record) == tuple:
+    if isinstance(token_record, tuple):
+        if isinstance(user_record, tuple):
             if token_record[2] == user_record[0]:
                 response['user'] = {}
                 response['user']['id'] = user_record[0]
                 response['user']['name'] = user_record[1]
                 response['user']['password'] = user_record[2]
             else:
-                response['error'] = {'message': 'the token and user do not match!'}
+                response['error'] = {
+                    'message': 'the token and user do not match!'}
         else:
             response['error'] = {'message': 'user id ' + user + ' not found'}
     else:
-        response['error'] = {'message': 'token id ' + str(token) + ' not found'}
+        response['error'] = {
+            'message': 'token id ' + str(token) + ' not found'}
     conn.close()
 
     return {'response': response}
+
 
 @route('/user', method='POST')
 def create_user():
     token = request.headers.get('X-Auth-Token')
     conn = sqlite3.connect('vAPI.db')
     c = conn.cursor()
-    token_query = "SELECT * FROM tokens WHERE token = '%s' AND userid = 10" % (str(token))
+    token_query = "SELECT * FROM tokens WHERE token = '%s' AND userid = 10" % (
+        str(token))
     c.execute(token_query)
     token_record = c.fetchone()
     response = {}
-    if type(token_record) == tuple:
+    if isinstance(token_record, tuple):
         data = request.json
         name = data['user']['username']
         password = data['user']['password']
@@ -176,14 +202,18 @@ def create_user():
             user_query = "SELECT * FROM users WHERE username = '%s'" % (name)
             c.execute(user_query)
             user_record = c.fetchone()
-            if type(user_record) == tuple:
-                response['error'] = {"message": "User %s already exists!" % name}
+            if isinstance(user_record, tuple):
+                response['error'] = {
+                    "message": "User %s already exists!" %
+                    name}
             else:
-                c.execute("INSERT INTO users (username, password) VALUES (?, ?)",  (name, password))
+                c.execute(
+                    "INSERT INTO users (username, password) VALUES (?, ?)", (name, password))
                 conn.commit()
                 response['user'] = {"username": name, "password": password}
         else:
-            response['error'] = {"message": "username {0} invalid format, check documentation!".format(name)}
+            response['error'] = {
+                "message": "username {0} invalid format, check documentation!".format(name)}
     else:
         response['error'] = {"message": "must provide valid admin token"}
 
@@ -200,12 +230,23 @@ def display_uptime(flag=None):
         command = "uptime"
     output = os.popen(command).read()
     response = {'response':
-        {
-            'Command': command,
-            'Output': output
-        }
-    }
+                {
+                    'Command': command,
+                    'Output': output
+                }
+                }
     return json.dumps(response, sort_keys=True, indent=2)
+
+
+@hook('after_request')
+def enable_cors():
+    '''
+    Method to enable cross origin resource sharing headers
+    for all requests.
+    '''
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = '*'
+    resp.headers['Access-Control-Allow-Headers'] = '*'
 
 debug(True)
 run(host='0.0.0.0', port=8081, reloader=True)
